9e1d0645d9a5089399cd5fcd96f886b3bc8aa658
kmitaebdata-ux <kmitaebdata@gmail.com>
2025-11-21 15:45:47 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1Aa_mnq_0g7ZEuLbYVjN62iCMWemlKUc",
+  authDomain: "kmit-marks-portal-9db76.firebaseapp.com",
+  projectId: "kmit-marks-portal-9db76",
+  storageBucket: "kmit-marks-portal-9db76.firebasestorage.app",
+  messagingSenderId: "264909025742",
+  appId: "1:264909025742:web:84de5216860219e6bc3b9f",
+  measurementId: "G-JMZ564P8PJ"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
